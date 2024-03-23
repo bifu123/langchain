9e360905efe323346de7dd23359e0825ca93778c
bifu123 <415135222@qq.com>
2024-03-23 09:50:34 +0800

    使用gemini api作语言模型

--- langchain_helper_gemini.py
+++ langchain_helper_gemini.py
@@ -23,54 +23,100 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 
 import os
+import shutil
 
 
-#################### 定义常量 ##################
-# 文档路径
+# 本地文档
 file_path = "./data/tesla_p40.pdf"
-# 矢量存储路径
+# 量化数据存储路径
 db_path = "./chroma_db"
 
-#################### 准备模型 ##################
-# 将GOOGLE_API_KEY加载到环境变量中
-os.environ['GOOGLE_API_KEY'] = '你的GEMINI API'
+os.environ['GOOGLE_API_KEY'] = '你的GEMINI API' #将GOOGLE_API_KEY加载到环境变量中
+
+
 # 量化模型
-embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-# embedding.embed_query("hello, world!")
-# 推理模型
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
+#embedding = OllamaEmbeddings(base_url="http://192.168.66.24:11434", model="nomic-embed-text") #线上google量化模型
+embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001") #本地ollama量化模型
+#embedding.embed_query("hello, world!")
+'''
+不同的量化模型量化结果是不一样的，原则是你选择哪家的量化模型，读取时必须使用相同的量化模型逆向读取，否则推理出错
+'''
 
-# embedding = OllamaEmbeddings(base_url="http://192.168.66.24:11434", model="nomic-embed-text")
-# llm = Ollama(base_url='http://192.168.66.26:11434', model="gemma:7b")
 
-#################### 加载文档 ##################
+# 线上模型
+#llm = Ollama(base_url='http://192.168.66.26:11434', model="test:7b") #本地ollama语言模型
+llm = ChatGoogleGenerativeAI(model="gemini-pro") #线上google gemini语言模型
+
+
+
 # 加载文档
 loader = PyPDFLoader(file_path)
 docs = loader.load()
-print(docs)
+# print(docs)
 
-#################### 分割文档 ##################
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+
+# 分割文档
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=128)
 all_splits = text_splitter.split_documents(docs)
-print(all_splits)
+# print(all_splits)
 
-#################### 量化存入 ##################
+
+#如果存在数据库目录则先删除
+def delete_chroma_db(db_path):
+    if os.path.exists(db_path):
+        if os.path.isfile(db_path):
+            os.remove(db_path)
+            print("File 'chroma_db' deleted.")
+        elif os.path.isdir(db_path):
+            shutil.rmtree(db_path)
+            print("Directory 'chroma_db' deleted.")
+    else:
+        print("File or directory 'chroma_db' does not exist.")
+
+# 调用函数以删除 'chroma_db'
+delete_chroma_db(db_path)
+
+#量化文档存入数据库
+vectorstore_to_db = Chroma.from_documents(
+    documents = all_splits,           # Data
+    embedding = embedding,        # Embedding model
+    persist_directory = db_path       # Directory to save data
+)
+
+
+#加载embedding
 vectorstore_from_db = Chroma(
     persist_directory = db_path,         # Directory of db
     embedding_function = embedding   # Embedding model
 )
 print(vectorstore_from_db)
 
+
+
+# # 准备问题
+# question="最大显存是多少？"
+# docs = vectorstore_from_db.similarity_search(question)
+# #print(docs)
+
+# #运行链
+# qachain=RetrievalQA.from_chain_type(llm, retriever=vectorstore_from_db.as_retriever())
+# ans = qachain.invoke({"query": "请用中文回答我：" + question})
+# print(ans["result"])
+'''
+以上写法并不兼容线上模型，仅对本地模型有用
+'''
+
+
+
 #################### 问答推理 ##################
-# 创建prompt模板
-template = """Answer the question a full sentence, based only on the following context and in Chinese:
+#创建prompt模板
+template = """Answer the question a full sentence, based only on the following context and tel me the answer in Chinese:
 {context}
 Question: {question}
 """
-
 #由模板生成prompt
-prompt = ChatPromptTemplate.from_template(template)
- 
+prompt = ChatPromptTemplate.from_template(template) 
+
 
 retriever=vectorstore_from_db.as_retriever()
 output_parser = StrOutputParser()
