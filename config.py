e30609dc7528b3c69b6355c5c01fef8d7289a4c5
bifu123 <415135222@qq.com>
2024-03-23 15:35:24 +0800

    更正错误

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from langchain_community.embeddings import OllamaEmbeddings # 量化文档
 #量化文档服务器
 oembed_server = OllamaEmbeddings(base_url="http://192.168.66.24:11434", model="nomic-embed-text")
 # LLM模型服务器
-ollama_server = Ollama(base_url='http://192.168.66.26:11434', model="llama2:7b")
+ollama_server = Ollama(base_url='http://192.168.66.26:11434', model="llava")
 #量化后数据保存路径
 db_path = "./chroma_db"
 #源文档路径
--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException, Request, Response
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 import shutil
 from fastapi import status
+from fastapi import Request
 from dal import *
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+# 接收POST传参
+
+
 templates = Jinja2Templates(directory="templates")
+
 
 # 设置文件存储目录
 file_directory = Path("data")
 file_directory.mkdir(parents=True, exist_ok=True)
 
-from fastapi import Request
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):  # 添加 request 参数
@@ -28,14 +35,57 @@ async def dummy_post():
     pass
 
 
-@app.get("/manager_files", response_class=HTMLResponse)
-async def read_files(request: Request):
-    files = list(file_directory.glob("*"))
-    return templates.TemplateResponse("manager_files.html", {"request": request, "files": files})
 
+
+
+
+
+
+
+
+# 文档管理
+def get_files_in_directory(directory: Path):
+    files = []
+    for item in directory.iterdir():
+        if item.is_file():
+            files.append(str(item.resolve()))  # 将文件的绝对路径添加到列表中
+            #files.append(str(item.relative_to(file_directory)))  # 将文件的相对路径添加到列表中
+        elif item.is_dir():
+            files.extend(get_files_in_directory(item))  # 递归获取子文件夹中的文件
+    return files
+
+@app.get("/manager_files")
+async def read_files(request: Request):
+    files_names = get_files_in_directory(file_directory)
+
+    files = list(file_directory.glob("*"))
+
+    # 获取文件名列表
+    # files_names = [file.name for file in file_directory.glob("*")]  
+    # 获取名为 "from" 的查询参数
+    from_value = request.query_params.get("from", None)  
+    if from_value == "qq":
+        return JSONResponse(content={"files": files_names})
+    
+    else:
+        return templates.TemplateResponse("manager_files.html", {"request": request, "files": files})
+
+
+
+
+
+
+
+
+
+# 上传文档
+'''
+{'name': '3E6A8972499BB1FBF59DF805A690DAB3.png', 'size': 121668, 'url': 'http://49.86.42.149/ftn_handler/b5b1fbd33abb0556b72184bfa6db902db8637a57f9696cb59a3114bc4f34ae9d16f72940f381e87af6f0e66d852ce1e5e0a0a0ae8f88c1c5c1d175464f6e8aab'}, 'user_id': 415135222}
+'''
+from fastapi import Request
 
 @app.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(request: Request, file: UploadFile = File(...)):
     # 将文件保存到指定目录
     file_path = file_directory / file.filename
     with open(file_path, "wb") as buffer:
@@ -43,7 +93,19 @@ async def upload_file(file: UploadFile = File(...)):
     #重新量化文档
     processor = DocumentProcessor(data_path, db_path, oembed_server)
     processor.update_database()
-    return RedirectResponse(url="/manager_files", status_code=status.HTTP_303_SEE_OTHER)
+    
+    from_value = request.query_params.get("from", None)  
+    if from_value == "qq":
+        return JSONResponse(content={"msg": "文档量化完成"})
+    else:
+        return RedirectResponse(url="/manager_files", status_code=status.HTTP_303_SEE_OTHER)
+
+
+
+
+
+
+
 
 
 @app.delete("/delete/{file_name}")
